4d22926a6362f429412dc8e511d11da140b868b5
johnburitto <79087305+johnburitto@users.noreply.github.com>
2022-06-06 23:40:55 +0300

    init

---
+++ src/main/java/com/johnburitto/interurbantransit/InterurbanTransitApplication.java
@@ -0,0 +1,13 @@
+package com.johnburitto.interurbantransit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterurbanTransitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InterurbanTransitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/johnburitto/interurbantransit/InterurbanTransitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.johnburitto.interurbantransit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterurbanTransitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
